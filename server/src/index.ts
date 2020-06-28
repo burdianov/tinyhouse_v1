6edc468adb21ceaefdf3d20fc7da524212981bc2
Lilian Burdianov <lilian.burdianov@gmail.com>
2020-06-28 20:58:11 +0400

    Create a post root to delete a listing

--- server/src/index.ts
+++ server/src/index.ts
@@ -5,10 +5,24 @@ import { listings } from './listings';
 const app = express();
 const port = 9000;
 
+app.use(express.json());
+
 app.get('/', (_req, res) => res.send('Just testing...'));
 
 app.get('/listings', (_req, res) => {
   return res.send(listings);
+});
+
+app.post('/delete-listing', (req, res) => {
+  const id: string = req.body.id;
+
+  for (let i = 0; i < listings.length; i++) {
+    if (listings[i].id === id) {
+      return res.send(listings.splice(i, 1));
+    }
+  }
+
+  return res.send('failed to delete listing');
 });
 
 app.listen(port, () => {
