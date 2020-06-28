72f129da275477ea57804f8e6aa0a989b483ab0e
Lilian Burdianov <lilian.burdianov@gmail.com>
2020-06-28 17:54:07 +0400

    test run express server

--- server/src/index.js
+++ server/src/index.js
@@ -1,1 +1,10 @@
-console.log('mudium');
+const express = require('express');
+
+const app = express();
+const port = 9000;
+
+app.get('/', (req, res) => res.send('Just testing...'));
+
+app.listen(port, () => {
+  console.log(`App running on http://localhost:${port}`);
+});
