ea2a7199fae8bcd39345171bd788fcf14c54df91
Lilian Burdianov <lilian.burdianov@gmail.com>
2020-06-28 20:47:46 +0400

    Create get listings route

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,9 +1,15 @@
 import express from 'express';
 
+import { listings } from './listings';
+
 const app = express();
 const port = 9000;
 
 app.get('/', (_req, res) => res.send('Just testing...'));
+
+app.get('/listings', (_req, res) => {
+  return res.send(listings);
+});
 
 app.listen(port, () => {
   console.log(`App running on http://localhost:${port}`);
