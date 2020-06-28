0ea426a2987484e7802eac693d7c32a0e998b03c
Lilian Burdianov <lilian.burdianov@gmail.com>
2020-06-28 17:59:58 +0400

    install and test nodemon

--- server/src/index.js
+++ server/src/index.js
@@ -3,7 +3,7 @@ const express = require('express');
 const app = express();
 const port = 9000;
 
-app.get('/', (req, res) => res.send('Just testing...'));
+app.get('/', (req, res) => res.send('Just testing... again'));
 
 app.listen(port, () => {
   console.log(`App running on http://localhost:${port}`);
