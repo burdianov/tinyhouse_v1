18e4636b8e1b81ca1e6f40d4bd37ae98e6780350
Lilian Burdianov <lilian.burdianov@gmail.com>
2020-06-28 20:30:14 +0400

    refactor index.ts

--- server/src/index.ts
+++ server/src/index.ts
@@ -3,7 +3,7 @@ import express from 'express';
 const app = express();
 const port = 9000;
 
-app.get('/', (req, res) => res.send('Just testing...'));
+app.get('/', (_req, res) => res.send('Just testing...'));
 
 app.listen(port, () => {
   console.log(`App running on http://localhost:${port}`);
