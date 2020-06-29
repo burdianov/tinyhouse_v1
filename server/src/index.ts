a91a9f3e856a81d1851ecdc495d614c1a399d6bd
Lilian Burdianov <lilian.burdianov@gmail.com>
2020-06-29 11:31:16 +0400

    minor refactoring

--- server/src/index.ts
+++ server/src/index.ts
@@ -8,14 +8,13 @@ const port = 9000;
 
 const mount = async (app: Application) => {
   const db = await connectDatabase();
+
   const server = new ApolloServer({
     typeDefs,
     resolvers,
     context: () => ({ db })
   });
   server.applyMiddleware({ app, path: '/api' });
-
-  app.use(express.json());
 
   app.listen(port, () => {
     console.log(`App running on http://localhost:${port}`);
