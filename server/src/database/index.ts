0ef4961d53e4c8e7df291685f0dd7107f9322260
Lilian Burdianov <lilian.burdianov@gmail.com>
2020-06-29 18:31:24 +0400

    Typify the connectDatabase function

--- server/src/database/index.ts
+++ server/src/database/index.ts
@@ -1,6 +1,8 @@
 import { MongoClient } from 'mongodb';
 import dotenv from 'dotenv';
 
+import { Database } from '../lib/types';
+
 dotenv.config();
 
 const user = process.env.MONGO_DB_USER_NAME;
@@ -10,7 +12,7 @@ const dbName = process.env.MONGO_DB_NAME;
 
 const url = `mongodb+srv://${user}:${userPassword}@${cluster}.mongodb.net/${dbName}?retryWrites=true&w=majority`;
 
-export const connectDatabase = async () => {
+export const connectDatabase = async (): Promise<Database> => {
   const client = await MongoClient.connect(url, {
     useNewUrlParser: true,
     useUnifiedTopology: true
