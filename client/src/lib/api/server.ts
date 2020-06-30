05657f05a5d00a519efd6091b3289bc1168f6136
Lilian Burdianov <lilian.burdianov@gmail.com>
2020-06-30 10:28:06 +0400

    Abstract the type of data from server fetch

--- client/src/lib/api/server.ts
+++ client/src/lib/api/server.ts
@@ -1,1 +1,17 @@
-export * from './server';
+interface Body {
+  query: string;
+}
+
+export const server = {
+  fetch: async <TData = any>(body: Body) => {
+    const res = await fetch('/api', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify(body)
+    });
+
+    return res.json() as Promise<{ data: TData }>;
+  }
+};
