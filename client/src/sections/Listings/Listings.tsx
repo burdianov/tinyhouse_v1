d3d474e5e11e58d6c404cf86f52561207622f7a5
Lilian Burdianov <lilian.burdianov@gmail.com>
2020-06-30 11:19:20 +0400

    Delete a listing

--- client/src/sections/Listings/Listings.tsx
+++ client/src/sections/Listings/Listings.tsx
@@ -1,7 +1,11 @@
 import React from 'react';
 
 import { server } from './../../lib/api';
-import { ListingsData } from './types';
+import {
+  ListingsData,
+  DeleteListingData,
+  DeleteListingVariables
+} from './types';
 
 const LISTINGS = `
   query Listings {
@@ -19,6 +23,14 @@ const LISTINGS = `
   }
 `;
 
+const DELETE_LISTING = `
+  mutation deleteListing($id: ID!) {
+    deleteListing(id: $id) {
+      id
+    }
+  }
+`;
+
 interface Props {
   title: string;
 }
@@ -29,10 +41,24 @@ export const Listings = ({ title }: Props) => {
     console.log(data);
   };
 
+  const deleteListing = async () => {
+    const { data } = await server.fetch<
+      DeleteListingData,
+      DeleteListingVariables
+    >({
+      query: DELETE_LISTING,
+      variables: {
+        id: '5efa36d2b6086a85fc130b2b'
+      }
+    });
+    console.log(data);
+  };
+
   return (
     <>
       <h1>{title}</h1>
       <button onClick={fetchListings}>Query Listings!</button>
+      <button onClick={deleteListing}>Delete a Listing!</button>
     </>
   );
 };
