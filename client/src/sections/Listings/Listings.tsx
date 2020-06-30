075ce9e096d651905a185e88f1979ad2f04ee070
Lilian Burdianov <lilian.burdianov@gmail.com>
2020-06-30 13:52:19 +0400

    Add loading and error state

--- client/src/sections/Listings/Listings.tsx
+++ client/src/sections/Listings/Listings.tsx
@@ -36,7 +36,7 @@ interface Props {
 }
 
 export const Listings = ({ title }: Props) => {
-  const { data, refetch } = useQuery<ListingsData>(LISTINGS);
+  const { data, loading, error, refetch } = useQuery<ListingsData>(LISTINGS);
 
   const deleteListing = async (id: string) => {
     await server.fetch<DeleteListingData, DeleteListingVariables>({
@@ -62,6 +62,14 @@ export const Listings = ({ title }: Props) => {
       })}
     </ul>
   );
+
+  if (loading) {
+    return <h2>Loading...</h2>;
+  }
+
+  if (error) {
+    return <h2>Something went wrong! Please try again later :(</h2>;
+  }
 
   return (
     <>
