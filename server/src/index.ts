f12b63dd1dfdcb78afaa488b4876581066a23d4e
Lilian Burdianov <lilian.burdianov@gmail.com>
2020-07-05 21:11:01 +0400

    Add user cookie on the server

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,4 +1,5 @@
 import express, { Application } from 'express';
+import cookieParser from 'cookie-parser';
 import { ApolloServer } from 'apollo-server-express';
 import dotenv from 'dotenv';
 import { typeDefs, resolvers } from './graphql';
@@ -11,10 +12,12 @@ const port = process.env.PORT || 9000;
 const mount = async (app: Application) => {
   const db = await connectDatabase();
 
+  app.use(cookieParser(process.env.SECRET));
+
   const server = new ApolloServer({
     typeDefs,
     resolvers,
-    context: () => ({ db })
+    context: ({ req, res }) => ({ db, req, res })
   });
   server.applyMiddleware({ app, path: '/api' });
 
